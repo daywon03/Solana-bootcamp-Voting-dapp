3a3bbe022b05a255580f1cd0394c185a9414a02d
Damon <badamon3@gmail.com>
2025-09-06 03:20:05 +0200

    Creation route API Blinks

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,6 +1,9 @@
-import { ActionGetResponse, ACTIONS_CORS_HEADERS } from "@solana/actions"
+import { ActionGetResponse, ActionPostRequest, ACTIONS_CORS_HEADERS } from "@solana/actions"
+import { Connection, PublicKey } from "@solana/web3.js";
+import { IconBrandLinkedin } from "@tabler/icons-react";
 
 
+export const OPTION  = GET;
 
 export async function GET(request: Request) {
 
@@ -9,7 +12,44 @@ export async function GET(request: Request) {
     title: "Vote for your favorite demon slayer character ",
     description:"Vote between Zenitsu and Gyomei",
     label: "Vote",
+    links: {
+      actions: [
+        {
+          type: "post",
+          label: "Vote Zenistu",
+          href: "/api/vote?candidate=Oscar"
+        },
+        {
+          type: "post",
+          label: "Vote Gyomei",
+          href: "/api/vote?candidate=Crunch"
+        }
+      ]
+    },
   };
   
   return  Response.json(actionMetadata, {headers: ACTIONS_CORS_HEADERS}); 
+}
+
+
+export async function POST(request: Request) {
+
+  const url = new URL(request.url);
+  const candidate = url.searchParams.get("candidate");// Vas permettre de vérifiée si candidat existe
+
+  if (candidate != "Oscar" && candidate != "Crunch"){
+    return new Response("Invalid candidate",{ status:400, headers: ACTIONS_CORS_HEADERS});
+  }
+  
+  const connection = new Connection("http://127.0.0.1:8899","confirmed");
+  const body : ActionPostRequest = await request.json()
+
+  let voter;
+
+  try {
+    //Doit renvoyé une public Key
+      voter = new PublicKey(body.account);
+  } catch (error) {
+    return new Response("Invalid account",{status:400, headers: ACTIONS_CORS_HEADERS});
+  }
 }
